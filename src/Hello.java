65ae84d676096a79b09412eb6b6f64e4c815b56a
Amir Jim-Daniels <ajimdaniels19@austincollege.edu>
2023-03-01 12:19:32 -0600

    initial commit

---
+++ src/Hello.java
@@ -0,0 +1,13 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Greeter mygreeter = new Greeter();
+		
+		mygreeter.greet();
+		
+	}
+
+}
